3aca7cebd0df992948d1ba7b5d6f0efd2847783e
nazifsohtaoglu <sohtanazif@gmail.com>
2022-02-26 12:23:01 +0300

    init

---
+++ src/constants.jsx
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = '0x7c4c9526d6d4A9985109A60c157Da16513da0BBa';
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
